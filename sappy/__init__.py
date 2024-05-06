4978d07aa4afcec0dfeaf075fb3b487358ade557
krispy-kenay <yannick.gachnang@gmail.com>
2024-05-06 11:19:19 +0200

    feat: setup for first package release

--- sappy/__init__.py
+++ sappy/__init__.py
@@ -1,1 +1,7 @@
+"""This package makes interacting with SAP from python easier by providing some streamlined functionality """
+
 from .client import Client
+
+__author__ = "Yannick G."
+__email__ = "study.gyannick@gmail.com"
+__version__ = "0.1"
